76f92f3ab6162f991ff3f1bbb19b966c3f36b004
Rahul Mahajan <aerorahul@users.noreply.github.com>
2023-07-06 15:33:03 -0400

    fix pynorms

--- docs/conf.py
+++ docs/conf.py
@@ -26,8 +26,6 @@ extensions = [
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
@@ -35,4 +33,5 @@ html_theme = 'sphinx_rtd_theme'
 html_theme_path = ["_themes", ]
 html_static_path = ['_static']
 
+# Show documentation from class as well as __init__ docstring
 autoclass_content = 'both'

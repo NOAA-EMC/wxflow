1652b54080c8471ce3cbf5456d39fabf41e2d5f8
DavidHuber <david.huber@noaa.gov>
2024-04-08 14:35:50 +0000

    hsi: better documentation; add helper function; catch empty args.

--- src/wxflow/hsi.py
+++ src/wxflow/hsi.py
@@ -1,5 +1,7 @@
 from .executable import which
 
+from typing import Union, List
+
 __all__ = ['Hsi']
 
 
@@ -17,42 +19,62 @@ class Hsi:
     >>> output = hsi.chgrp("rstprod", "/HPSS/pth/to/some_file") # Change the group to rstprod
     """
 
-    def __init__(self, def_hsi_args: list = ["-q", "-e"]):
+    def __init__(self, quiet: bool = True, echo_commands:bool = True, opts: Union[str, List] = []):
         """Instantiate the hsi command
 
-        def_hsi_args: str
-            List of default arguments to send to hsi.  The defaults are
-            -q: run in quiet mode (do not print login information)
-            -e: echo each command
+        Parameters:
+        -----------
+        quiet : bool
+                Run hsi in quiet mode (suppress login information, transfer info, etc)
+        echo_commands : bool
+                Echo each command.  Some commands will still not echo (e.g. chmod).
+        opts : str | list
+                Additional arguments to send to each hsi command.
         """
 
         self.exe = which("hsi", required=True)
 
-        for arg in def_hsi_args:
+        hsi_args = []
+
+        if quiet:
+            hsi_args.append("-q")
+
+        if echo_commands:
+            hsi_args.append("-e")
+
+        hsi_args.extend(Hsi._split_opts(hsi_args))
+
+        for arg in hsi_args:
             self.exe.add_default_arg(arg)
 
     def _hsi(self, arg_list: list, silent: bool = False, ignore_errors: list = []) -> str:
         """Direct command builder function for hsi based on the input arguments.
 
-        arg_list: list
-            A list of arguments to sent to hsi
+        Parameters:
+        arg_list : list
+                A list of arguments to sent to hsi
 
-        silent: bool
-            Whether the output of the hsi command should be written to stdout
+        silent : bool
+                Whether the output of the hsi command should be written to stdout
 
-        ignore_errors: list
-            List of error numbers to ignore.  For example, hsi returns error
-            number 64 if a target file does not exist on HPSS.
+        ignore_errors : list
+                List of error numbers to ignore.  For example, hsi returns error
+                number 64 if a target file does not exist on HPSS.
 
-        Return: str
-            Concatenated output and error of the hsi command.
+        Returns
+        -------
+        output : str
+                Concatenated output and error of the hsi command.
 
         Example:
         --------
             >>> hsi = Hsi()
             >>> # Execute `hsi get some_local_file : /some/hpss/file`
-            >>> hsi.hsi(["get","some_local_file : /some/hpss/file"])
+            >>> hsi._hsi(["get","some_local_file : /some/hpss/file"])
         """
+
+        # Remove any empty arguments which can cause issues for hsi
+        arg_list = [arg for arg in arg_list if arg != ""]
 
         if silent:
             output = self.exe(*arg_list, output=str, error=str,
@@ -63,7 +85,7 @@ class Hsi:
 
         return output
 
-    def get(self, source: str, target: str = "", hsi_flags: str = "") -> str:
+    def get(self, source: str, target = None, opts: Union[List, str] = []) -> str:
         """ Method to get a file from HPSS via hsi
 
         Parameters
@@ -75,31 +97,31 @@ class Hsi:
                 Location on the local machine to place the file.  If not specified,
                 then the file will be placed in the current directory.
 
-        hsi_flags : str
-                String of flags to send to hsi. By default, suppress login info and
-                echo the get command.
+        opts : str | list
+                List or string of additional options to send to hsi command.
+
+        Returns
+        -------
+        output : str
+                Concatenated output and error of the hsi get command.
         """
         arg_list = []
 
         # Convert to str to handle Path objects
-        target = str(target)
         source = str(source)
+        target = str(target) if target is not None else None
 
-        # Parse any hsi flags
-        if len(hsi_flags) > 0:
-            arg_list.extend(hsi_flags.split(" "))
+        # Parse any hsi options
+        arg_list.extend(Hsi._split_opts(opts))
 
         arg_list.append("get")
-        if len(target) == 0:
-            arg_list.append(source)
-        else:
-            arg_list.append(target + " : " + source)
+        args_list.append(source) if target is None else arg_list.append(f"{target} : {source}")
 
         output = self._hsi(arg_list)
 
         return output
 
-    def put(self, source: str, target: str, hsi_flags: str = "",
+    def put(self, source: str, target: str, opts: Union[List, str] = [],
             listing_file: str = None) -> str:
         """ Method to put a file onto HPSS via hsi
 
@@ -111,9 +133,13 @@ class Hsi:
         target : str
                 Full path of the target location of the file on HPSS.
 
-        hsi_flags : str
-                String of flags to send to hsi. By default, suppress login info
-                and echo the put command.
+        opts : str | List
+                List or string of additional options to send to hsi.
+
+        Returns
+        -------
+        output : str
+                Concatenated output and error of the hsi put command.
         """
         arg_list = []
 
@@ -121,9 +147,8 @@ class Hsi:
         target = str(target)
         source = str(source)
 
-        # Parse any hsi flags
-        if len(hsi_flags) > 0:
-            arg_list.extend(hsi_flags.split(" "))
+        # Parse any hsi options
+        arg_list.extend(Hsi._split_opts(opts))
 
         arg_list.append("put")
         arg_list.append(source + " : " + target)
@@ -131,8 +156,8 @@ class Hsi:
 
         return output
 
-    def chmod(self, mod: str, target: str, hsi_flags: str = "",
-              chmod_flags: str = "") -> str:
+    def chmod(self, mod: str, target: str, hsi_opts: Union[List, str] = "",
+              chmod_opts: Union[List, str] = "") -> str:
         """ Method to change the permissions of a file or directory on HPSS
 
         Parameters
@@ -144,23 +169,27 @@ class Hsi:
         target : str
                 Full path of the target location of the file on HPSS.
 
-        hsi_flags : str
-                String of flags to send to hsi. By default, suppress login info.
+        hsi_opts : list | str
+                Options to send to hsi.
 
-        chmod_flags : str
-                Flags to send to chmod. See "hsi chmod -?" for more details.
+        chmod_opts : list | str
+                Options to send to chmod. See "hsi chmod -?" for more details.
+
+        Return
+        ------
+        output : str
+                Concatenated output and error of the hsi chmod command.
         """
 
         arg_list = []
 
-        # Parse any hsi flags
-        if len(hsi_flags) > 0:
-            arg_list.extend(hsi_flags.split(" "))
+        # Parse any hsi options
+        arg_list.extend(Hsi._split_opts(hsi_opts))
 
         arg_list.append("chmod")
 
-        if len(chmod_flags) > 0:
-            arg_list.extend(chmod_flags.split(" "))
+        # Parse any chmod options
+        arg_list.extend(Hsi._split_opts(chmod_opts))
 
         arg_list.append(mod)
         arg_list.append(target)
@@ -168,8 +197,8 @@ class Hsi:
 
         return output
 
-    def chgrp(self, group_name: str, target: str, hsi_flags: str = "",
-              chgrp_flags: str = "") -> str:
+    def chgrp(self, group_name: str, target: str, hsi_opts: str = "",
+              chgrp_opts: str = "") -> str:
         """ Method to change the group of a file or directory on HPSS
 
         Parameters
@@ -180,23 +209,27 @@ class Hsi:
         target : str
                 Full path of the target location of the file on HPSS.
 
-        hsi_flags : str
-                String of flags to send to hsi. By default, suppress login info.
+        hsi_opts : str
+                String of options to send to hsi.
 
-        chgrp_flags : str
-                Flags to send to chgrp.  See "hsi chgrp -?" for more details.
+        chgrp_opts : str
+                Options to send to chgrp.  See "hsi chgrp -?" for more details.
+
+        Returns
+        -------
+        output : str
+                Concatenated output and error of the hsi chgrp command.
         """
 
         arg_list = []
 
-        # Parse any hsi flags
-        if len(hsi_flags) > 0:
-            arg_list.extend(hsi_flags.split(" "))
+        # Parse any hsi options
+        arg_list.extend(Hsi._split_opts(hsi_opts))
 
         arg_list.append("chgrp")
 
-        if len(chgrp_flags) > 0:
-            arg_list.extend(chgrp_flags.split(" "))
+        # Parse any chgrp options
+        arg_list.extend(Hsi._split_opts(chgrp_opts))
 
         arg_list.append(group_name)
         arg_list.append(target)
@@ -204,7 +237,7 @@ class Hsi:
 
         return output
 
-    def rm(self, target: str, hsi_flags: str = "", rm_flags: str = "") -> str:
+    def rm(self, target: str, hsi_opts: str = "", rm_opts: str = "") -> str:
         """ Method to delete a file or directory on HPSS via hsi
 
         Parameters
@@ -212,30 +245,34 @@ class Hsi:
         target : str
                 Full path of the target location of the file on HPSS.
 
-        hsi_flags : str
-                String of flags to send to hsi. By default, suppress login info
-                and echo the rm command.
+        hsi_opts : str
+                String of options to send to hsi.
 
-        rm_flags : str
-                Flags to send to rm.  See "hsi rm -?" for more details.
+        rm_opts : str
+                Options to send to rm.  See "hsi rm -?" for more details.
+
+        Returns
+        -------
+        output : str
+                Concatenated output and error of the hsi rm command.
         """
 
         # Call rmdir if recursive (-r) flag present
-        if "-r" in rm_flags:
-            rmdir_flags = rm_flags.replace("-r", "")
-            output = self.rmdir(target, hsi_flags, rmdir_flags)
+        # NOTE this will ONLY remove empty directories
+        if "-r" in rm_opts:
+            rmdir_opts = [ opt for opts in Hsi._split_opts(rm_opts) if opt != "-r" ]
+            output = self.rmdir(target, hsi_opts, rmdir_opts)
             return output
 
         arg_list = []
 
-        # Parse any hsi flags
-        if len(hsi_flags) > 0:
-            arg_list.extend(hsi_flags.split(" "))
+        # Parse any hsi options
+        arg_list.extend(Hsi._split_opts(hsi_opts))
 
         arg_list.append("rm")
 
-        if len(rm_flags) > 0:
-            arg_list.extend(rm_flags.split(" "))
+        # Parse any rm options
+        arg_list.extend(Hsi._split_opts(rm_opts))
 
         arg_list.append(target)
 
@@ -244,39 +281,42 @@ class Hsi:
 
         return output
 
-    def rmdir(self, target: str, hsi_flags: str = "", rmdir_flags: str = "") -> str:
-        """ Method to delete a directory on HPSS via hsi
+    def rmdir(self, target: str, hsi_opts: str = "", rmdir_opts: str = "") -> str:
+        """ Method to delete an empty directory on HPSS via hsi
 
         Parameters
         ----------
         target : str
                 Full path of the target location of the file on HPSS.
 
-        hsi_flags : str
-                String of flags to send to hsi. By default, suppress login info
-                and echo the rmdir command.
+        hsi_opts : str
+                String of options to send to hsi.
 
-        rmdir_flags : str
-                Flags to send to rmdir.  See "hsi rmdir -?" for more details.
+        rmdir_opts : str
+                Options to send to rmdir.  See "hsi rmdir -?" for more details.
+
+        Returns
+        -------
+        output : str
+                Concatenated output and error of the hsi rmdir command.
         """
 
         arg_list = []
 
-        # Parse any hsi flags
-        if len(hsi_flags) > 0:
-            arg_list.extend(hsi_flags.split(" "))
+        # Parse any hsi options
+        arg_list.extend(Hsi._split_opts(hsi_opts))
 
         arg_list.append("rmdir")
 
-        if len(rmdir_flags) > 0:
-            arg_list.extend(rmdir_flags.split(" "))
+        # Parse any rmdir options
+        arg_list.extend(Hsi._split_opts(rmdir_opts))
 
         arg_list.append(target)
         output = self._hsi(arg_list)
 
         return output
 
-    def mkdir(self, target: str, hsi_flags: str = "", mkdir_flags: str = "") -> str:
+    def mkdir(self, target: str, hsi_opts: str = "", mkdir_opts: str = "") -> str:
         """ Method to delete a file or directory on HPSS via hsi
 
         Parameters
@@ -284,45 +324,53 @@ class Hsi:
         target : str
                 Full path of the target location of the file on HPSS.
 
-        hsi_flags : str
-                String of flags to send to hsi. By default, suppress login info
-                and echo the mkdir command.
+        hsi_opts : str
+                String of options to send to hsi.
+
+        Returns
+        -------
+        output : str
+                Concatenated output and error of the hsi mkdir command.
         """
 
         arg_list = []
 
-        # Parse any hsi flags
-        if len(hsi_flags) > 0:
-            arg_list.extend(hsi_flags.split(" "))
+        # Parse any hsi options
+        arg_list.extend(Hsi._split_opts(hsi_opts))
 
         # The only flag available for mkdir is -p, which we will use.
         arg_list.extend(["mkdir", "-p"])
 
-        if len(mkdir_flags) > 0:
-            arg_list.extend(mkdir_flags.split(" "))
+        # Parse any mkdir options
+        arg_list.extend(Hsi._split_opts(mkdir_opts))
 
         arg_list.append(target)
         output = self._hsi(arg_list)
 
         return output
 
-    def ls(self, target: str, hsi_flags: str = "", ls_flags: str = "",
+    def ls(self, target: str, hsi_opts: str = "", ls_opts: str = "",
            ignore_missing: bool = False) -> str:
         """ Method to list files/directories on HPSS via hsi
 
         Parameters
         ----------
         target : str
-            Full path of the target location on HPSS.
+                Full path of the target location on HPSS.
 
-        hsi_flags : str
-            String of flags to send to hsi. By default, suppress login info.
+        hsi_opts : str
+                String of options to send to hsi.
 
-        ls_flags : str
-            Flags to send to ls.  See "hsi ls -?" for more details.
+        ls_opts : str
+                Options to send to ls.  See "hsi ls -?" for more details.
 
-        ignore_missing: bool
-            Flag to ignore missing files
+        ignore_missing : bool
+                Flag to ignore missing files
+
+        Returns
+        -------
+        output : str
+                Concatenated output and error of the hsi ls command.
         """
 
         arg_list = []
@@ -332,15 +380,13 @@ class Hsi:
         else:
             ignore_errors = []
 
-        # Parse any hsi flags
-        if len(hsi_flags) > 0:
-            arg_list.extend(hsi_flags.split(" "))
+        # Parse any hsi options
+        arg_list.extend(Hsi._split_opts(hsi_opts))
 
         arg_list.append("ls")
 
-        # Parse any ls flags
-        if len(ls_flags) > 0:
-            arg_list.extend(ls_flags.split(" "))
+        # Parse any ls options
+        arg_list.extend(Hsi._split_opts(ls_opts))
 
         arg_list.append(target)
         output = self._hsi(arg_list, ignore_errors=ignore_errors)
@@ -355,7 +401,9 @@ class Hsi:
         target : str
                 Full path of the target location on HPSS.
 
-        Return: bool
+        Returns
+        -------
+        pattern_exists : bool
                 True if the target exists on HPSS.
         """
 
@@ -365,9 +413,30 @@ class Hsi:
         output = self._hsi(arg_list, silent=True, ignore_errors=[64])
 
         if "HPSS_ENOENT" in output:
-            return False
+            pattern_exists = False
         # Catch wildcards
         elif f"Warning: No matching names located for '{target}'" in output:
-            return False
+            pattern_exists = False
         else:
-            return True
+            pattern_exists = True
+
+        return pattern_exists
+
+    @staticmethod
+    def _split_opts(opts: Union[List, str] = "") -> list:
+        """ Method to split input list or string of hsi options
+
+        Parameters
+        ----------
+        opts : list | str
+                Input list or string of options to send to hsi or subcommand
+
+        Returns
+        -------
+        split_opts : list
+                List of options to send to hsi or the hsi subcommand
+        """
+
+        split_opts = opts.split(" ") if isinstance(opts, str) else opts
+
+        return split_opts

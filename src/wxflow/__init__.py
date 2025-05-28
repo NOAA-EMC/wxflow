695bde98451cea324e1834e960fb06580652a598
Rahul Mahajan <aerorahul@users.noreply.github.com>
2025-05-28 15:48:29 -0400

    prepare for release 0.3.0

--- src/wxflow/__init__.py
+++ src/wxflow/__init__.py
@@ -21,5 +21,5 @@ from .yaml_file import (YAMLFile, dump_as_yaml, parse_j2yaml, parse_yaml,
                         save_as_yaml, vanilla_yaml)
 
 __docformat__ = "restructuredtext"
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 wxflow_directory = os.path.dirname(__file__)

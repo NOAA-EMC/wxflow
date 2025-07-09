6da0149430fd2f9fa1cc1ea1d1d55f0f9716fe93
DavidNew-NOAA <david.new@noaa.gov>
2025-07-09 19:04:17 +0000

    Add test coverage for workflow exceptions

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1,35 +1,86 @@
 import pytest
 
-from wxflow import WorkflowException
+from wxflow import WorkflowException, WorkflowKeyError, WorkflowTypeError
 
 # ----
 
 
-class TestError(WorkflowException):
+class TestWorkflowException(WorkflowException):
     """
     Description
     -----------
 
-    This is the base-class for exceptions encountered within the
-    wxflow/errors unit-tests module; it is a sub-class of Error.
+    This is the base-class for generic exceptions encountered within the
+    wxflow/errors unit-tests module; it is a sub-class of WorkflowException.
+
+    """
+
+class TestWorkflowKeyError(WorkflowKeyError):
+    """
+    Description
+    -----------
+
+    This is the base-class for KeyError exceptions encountered within the
+    wxflow/errors unit-tests module; it is a sub-class of WorkflowKeyError.
+
+    """
+
+class TestWorkflowTypeError(WorkflowTypeError):
+    """
+    Description
+    -----------
+
+    This is the base-class for TypeError exceptions encountered within the
+    wxflow/errors unit-tests module; it is a sub-class of WorkflowTypeError.
 
     """
 
 # ----
 
-
-def test_errors() -> None:
+def test_workflow_exception() -> None:
     """
     Description
     -----------
 
-    This function provides a unit test for the errors module.
+    This function provides a unit test for the WorkflowException class.
 
     """
 
     # Raise the base-class exception.
     with pytest.raises(Exception):
-        msg = "Testing exception raise."
-        raise TestError(msg=msg)
+        msg = "Testing WorkflowException raise."
+        raise TestWorkflowException(msg=msg)
+
+    assert True
+
+def test_workflow_key_error() -> None:
+    """
+    Description
+    -----------
+
+    TThis function provides a unit test for the WorkflowKeyError class.
+
+    """
+
+    # Raise the base-class exception.
+    with pytest.raises(Exception):
+        msg = "Testing WorkflowKeyError raise."
+        raise TestWorkflowKeyError(msg=msg)
+
+    assert True
+
+def test_workflow_type_error() -> None:
+    """
+    Description
+    -----------
+
+    TThis function provides a unit test for the WorkflowTypeError class.
+
+    """
+
+    # Raise the base-class exception.
+    with pytest.raises(Exception):
+        msg = "Testing WorkflowTypeError raise."
+        raise TestWorkflowTypeError(msg=msg)
 
     assert True

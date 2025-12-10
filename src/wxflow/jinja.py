2e597cf00d4fca073dedd58755d09dba6a2fb15d
Rahul Mahajan <aerorahul@users.noreply.github.com>
2025-12-10 12:52:33 -0500

    add a helper utility to parse jinja templates and optionally save them

--- src/wxflow/jinja.py
+++ src/wxflow/jinja.py
@@ -260,3 +260,27 @@ class Jinja:
         None
         """
         sys.stdout.write(self.render)
+
+
+def parse_j2tmpl(template_path_or_string: str, data: Dict, output_file=None):
+        """
+        Description
+        -----------
+        Render a jinja2 template and optionally write the output to file
+        Parameters
+        -----------
+        template_path_or_string : str
+            Path to the template file or a templated string
+        data : dict
+            Data to be substituted into the template
+        output_file: str
+        Path to the output file
+        Returns
+        -------
+        None
+        """
+        jobj = Jinja(template_path_or_string, data)
+        if output_file is None:
+            return jobj.render
+        else:
+            jobj.save(output_file)

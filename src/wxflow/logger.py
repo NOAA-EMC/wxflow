43404d5048ea0aa23ffd01c7212c087fd67f0390
David Huber <david.huber@noaa.gov>
2026-04-27 13:50:36 -0400

    Do not log 'self'

--- src/wxflow/logger.py
+++ src/wxflow/logger.py
@@ -242,7 +242,8 @@ def logit(logger: logging.Logger, name: str = None, message: str = None):
         @wraps(func)
         def wrapper(*args, **kwargs):
 
-            passed_args = [repr(aa) for aa in args]
+            # Get all of the arguments passed to the function and log them, skipping the self argument if it exists.
+            passed_args = [repr(aa) for aa in args if aa != 'self']
             passed_kwargs = [f"{kk}={repr(vv)}" for kk, vv in list(kwargs.items())]
 
             call_msg = 'BEGIN: ' + log_msg
